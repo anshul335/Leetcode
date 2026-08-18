8001692cda68f410b5718382f354537c36f85943
anshul335 <anshulagarwal337@gmail.com>
2026-08-18 19:33:11 +0530

    Time: 0 ms (100%), Space: 38.3 MB (70.45%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        vector<int>freq(26,0);
+        int count = 0;
+        for(int i =0;i<tasks.size();i++){
+            freq[tasks[i]-'A']++;
+            count = max(count,freq[tasks[i]-'A']);
+
+        }
+        int ans = (count-1)*(n+1);
+        for(auto e:freq){
+            if(e==count)ans++;
+        }
+        return max((int)tasks.size(),ans);
+    }
+};
